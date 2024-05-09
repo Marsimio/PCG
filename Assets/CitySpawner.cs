e409e06e451ac645d003a6532d97874a89058e17
Lydon Camilleri <Stardustlion0@gmail.com>
2024-05-09 17:50:02 +0200

    Task 2
    Random Layout Generation

--- Assets/CitySpawner.cs
+++ Assets/CitySpawner.cs
@@ -1,33 +1,31 @@
 using UnityEngine;
 
-public class CitySpawner: MonoBehaviour
+public class CitySpawner : MonoBehaviour
 {
 
     public int rows = 10;
     public int columns = 10;
     public float spacing = 200;
 
-    private int[,] gridLayout = new int[,]
-    {
-        { 1, 2, 1, 2, 2, 2, 1, 2, 1, 2 },
-        { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
-        { 1, 2, 1, 2, 2, 2, 1, 2, 1, 2 },
-        { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
-        { 1, 2, 1, 2, 2, 2, 1, 2, 1, 2 },
-        { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
-        { 1, 2, 1, 2, 2, 2, 1, 2, 1, 2 },
-        { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
-        { 1, 2, 1, 2, 2, 2, 1, 2, 1, 2 },
-        { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 }
-    };
 
     void Start()
     {
-        BuildGrid();
+        GenerateLevel();
     }
 
-    void BuildGrid()
+    void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ClearGrid();
+            GenerateLevel();
+        }
+    }
+
+    void GenerateLevel()
+    {
+        int[,] gridLayout = GenerateRandomLayout();
+
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < columns; j++)
@@ -35,20 +33,46 @@ public class CitySpawner: MonoBehaviour
                 Vector3 position = new Vector3(j * 200, 0, i * 200);
                 print(position);
                 GameObject gameObject = new GameObject();
-                gameObject.transform.position = position; 
-                switch (gridLayout[i, j])
+                gameObject.transform.position = position;
+
+                if (gridLayout[i, j] == 1)
                 {
-                    case 1:
-                        gameObject.name = "House";
-                        gameObject.AddComponent<House>();
-                        break;
-                    case 2:
-                        gameObject.name = "Road";
-                        gameObject.AddComponent<Road>();
-                        break;
+                    gameObject.name = "House";
+                    gameObject.AddComponent<House>();
+                }
+                else if (gridLayout[i, j] == 2)
+                {
+                    gameObject.name = "Road";
+                    gameObject.AddComponent<Road>();
                 }
                 gameObject.transform.parent = this.transform;
             }
+        }
+    }
+
+    int[,] GenerateRandomLayout()
+    {
+        int[,] layout = new int[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                int rand = Random.Range(0, 2);
+                if (rand == 0) 
+                    layout[i, j] = 1;
+                else if (rand == 1) 
+                    layout[i, j] = 2; 
+            }
+        }
+
+        return layout;
+    }
+    void ClearGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
         }
     }
 }
